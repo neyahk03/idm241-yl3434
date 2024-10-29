e38d64c324a0e0fde7c6d700123537d1594f9fcb
Yen Luong <yl3434@drexel.edu>
2024-10-29 01:37:03 -0400

    :construction: Updated build.html

--- script/script.js
+++ script/script.js
@@ -1,15 +1,22 @@
+// const flipCard = document.getElementById('flip-card');
+// const unflipButton = document.getElementById('unflip-button');
+
+// flipCard.addEventListener('click', (event) => {
+//   // Prevent flipping if the "X" button is clicked
+//   if (event.target !== unflipButton) {
+//     flipCard.classList.add('flipped');
+//   }
+// });
+
+// unflipButton.addEventListener('click', (event) => {
+//   // Prevent the event from bubbling up to the card container
+//   event.stopPropagation();
+//   flipCard.classList.remove('flipped');
+// });
+
 const flipCard = document.getElementById('flip-card');
-const unflipButton = document.getElementById('unflip-button');
 
-flipCard.addEventListener('click', (event) => {
-  // Prevent flipping if the "X" button is clicked
-  if (event.target !== unflipButton) {
-    flipCard.classList.add('flipped');
-  }
+flipCard.addEventListener('click', () => {
+  flipCard.classList.toggle('flipped');
 });
 
-unflipButton.addEventListener('click', (event) => {
-  // Prevent the event from bubbling up to the card container
-  event.stopPropagation();
-  flipCard.classList.remove('flipped');
-});
