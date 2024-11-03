9efa107577e0a8dbb4ef35fd039b6d6cebd1355b
Yen Luong <yl3434@drexel.edu>
2024-11-03 16:15:41 -0500

    :construction: Updated beta/build.html and css

--- script/script.js
+++ script/script.js
@@ -14,9 +14,29 @@
 //   flipCard.classList.remove('flipped');
 // });
 
-const flipCard = document.getElementById('flip-card');
+// const flipCard = document.getElementById('flip-card');
 
-flipCard.addEventListener('click', () => {
-  flipCard.classList.toggle('flipped');
+// flipCard.addEventListener('click', () => {
+//   flipCard.classList.toggle('flipped');
+// });
+
+
+// const play = document.getElementById('play');
+
+// play.addEventListener('mouseup', () => {
+//   window.open('https://www.crunchyroll.com/series/GRMG8ZQZR/one-piece', '_blank');
+// })
+
+const flipCard = document.getElementById('flip-card');
+const play = document.getElementById('play');
+
+flipCard.addEventListener('click', (event) => {
+  if (!event.target.closest('#play')) {
+    flipCard.classList.toggle('flipped');
+  }
 });
 
+play.addEventListener('mouseup', (event) => {
+  event.stopPropagation(); // Prevent the event from propagating to the flip card click
+  window.open('https://www.crunchyroll.com/watch/GR3VWXP96/im-luffy-the-man-whos-gonna-be-king-of-the-pirates', '_blank');
+});
